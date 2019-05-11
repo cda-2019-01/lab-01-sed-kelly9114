968cc7b19cd152abefac529a8cacb05105c1c4fd
kelly <kesalazarr@unal.edu.co>
2019-05-10 19:07:31 -0500

    entrega laboratorio1

--- script.sh
+++ script.sh
@@ -1,7 +1,13 @@
-sed -e 's#\([0-9][0-9]\)/\([0-9][0-9]\)/\([0-9][0-9]\)#20\3-\2-\1#g' data.csv > out.1
-sed -e '4 s#N#\\N#' out.1 > out.2
-sed -e '6 s#N#\\N#' out.2 > out.3
-sed 's/,/./g' out.3 > out.4
-sed 's/;/,/g' out.4 > out.5
-sed -n '1,3p; 11p' out.5 > out.6
+sed 's#\([0-9][0-9]\)/\([0-9][0-9]\)/\([0-9][0-9]\)#20\3-\2-\1#g' data.csv > out.1
 
+sed  's#;[a-z,N]#;\\N#' out.1 > out.2
+
+sed -e 's/,/./g' out.2 > out.3
+
+sed -e 's/;/,/g' out.3 > out.4
+
+grep -v \N  out.4 > out.5
+
+cat out.5
+
+rm out.*
