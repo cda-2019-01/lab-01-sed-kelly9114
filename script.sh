6de82f0ee74711f9774c2b97c3749b4d7dd5a0fd
kelly <kesalazarr@unal.edu.co>
2019-05-19 15:07:45 -0500

    entrega_final_lab1

--- script.sh
+++ script.sh
@@ -1,13 +1,21 @@
-sed 's#\([0-9][0-9]\)/\([0-9][0-9]\)/\([0-9][0-9]\)#20\3-\2-\1#g' data.csv > out.1
 
-sed  's#;[a-z,N]#;\\N#' out.1 > out.2
+sed -e ' s#\([0-9][0-9]\)/\([0-9][0-9]\)/\([0-9][0-9]\)#20\3-\2-\1#g' data.csv > out.1
 
-sed -e 's/,/./g' out.2 > out.3
+sed -e ' s#\([0-9]\)/\([0-9]\)/\([0-9][0-9][0-9][0-9]\)#\3-0\2-0\1#g'  out.1 > out.2
 
-sed -e 's/;/,/g' out.3 > out.4
+sed -e ' s#;N#;\\N#g' -e ' s#;n#;\\N#g' -e ' s#;\\n#;\\N#g' -e ' s#;;#;\\N;#g' -e 's#;""#;\\N#g'   out.2 > out.3
 
-grep -v \N  out.4 > out.5
+sed -e 's/,/./g' -e 's/;/,/g' out.3 > out.4
 
-cat out.5
+sed -e 's#,\\N,\\N,#,\\N,\\N,\\N#g' -e 's#,C,\\N,#,C,\\N,\\N#g' out.4 > out.5
 
-rm out.*
+sed 's/[a-c,n]/\U&/g' out.5 > out.6
+
+sed 's/,200.0/,200,0/' out.6 > out.7
+
+sed 's/2013-08-02/2014-08-02/' out.7 > final.csv
+
+cat final.csv
+
+rm  out*
+
